b396b1e7a07b1bd84bdc9d528ca14e889e72b686
saharassss <sasharasheva@gmail.com>
2024-02-05 16:13:49 +0300

    оптимизировали задачу

--- task_3/main.js
+++ task_3/main.js
@@ -1,61 +1,93 @@
 let college = [
     {
-        name: "harry",
+        name: "Harry",
         age: 18,
-        group: "ис-2-1",
+        group: "ИС-2-1",
         studID: 321232,
-        typeLearning: "очная",
-        task: ["физра", "мдк", "мдк02"],
-        cash: 5500,
-        isStudent: true
+        typeLearning: "Очная",
+        task: ["Физ-ра", "МДК-02-02", "МДК-02-02", "МДК-04-01"],
+        cash: 0,
+        isStudent: true   // указываем, что атрибуты сверху принадлежат студенту
     },
-
     {
-        name: "ron",
+        name: "Ron",
         age: 19,
-        group: "ис-2-2",
-        studID: 321132,
-        typeLearning: "заочная",
-        task: ["физика", "мдк01", "мдк03"],
+        group: "ИС-2-2",
+        studID: 321233,
+        typeLearning: "Заочная",
+        task: ["ИЗО", "МДК-03-02", "ПРАВО", "МДК-04-01"],
         cash: 0,
         isStudent: true
     },
-
     {
-        name: "tom",
+        name: "Tom",
         age: 32,
-        iskurator: true,
-        kuratorGroup: "ис-2-2",
-        kafedra: "Инф.тех",
-        predmet: ["мдк", "мдк1", "мдк2"],
-        isStudent: false,
+        isKurator: true,
+        kuratorGroup: "ИС-2-2",
+        kafedra: "ПОВТ",
+        typeLearning: "Очная",
+        lesson: ["МДК-02-02", "МДК-02-02", "МДК-04-01", "МДК-03-03"],
+        isStudent: false
     },
     {
-        name: "bob",
-        age: 33,
-        iskurator: true,
-        kuratorGroup: "ис-2-1",
-        kafedra: "Инф.тех",
-        predmet: ["мдк", "мдк1", "мдк2"],
-        isStudent: false,
+        name: "Bob",
+        age: 32,
+        isKurator: true,
+        kuratorGroup: "ИС-2-1",
+        kafedra: "ПОВТ",
+        typeLearning: "Очная",
+        lesson: ["МДК-02-02", "МДК-02-02", "МДК-04-01", "МДК-03-03"],
+        isStudent: false
     }
-
 ];
 
+let count = 1
+let personAge = college[count]["age"]
+let personName = college[count]["name"]
+let personcash = college[count]["cash"]
+let student = college[count]["isStudent"]
+let kurator = college[count]["isKurator"]
+let kuratorGroup = college[count]["iskuratorGroup"]
+
 if (college[0]["isStudent"] === true) {
-    console.log(`Вы выбрали студента ${college[0]["name"]}`);
-    if (college[0]["age"] >= 18) {
-        console.log(`Студент ${college[0]["name"]} совершеннолетний. Ему ${college[0]["age"]}`);
+    console.log(`Вы выбрали студента ${college[count]["name"]}`);
+    if (college[count]["age"] >= 18) {
+        console.log(`Студент ${college[count]["name"]} совершеннолетний, ему ${college[count]["age"]}`);
+    }
+    if (studen === true) {
+        console.log(`студент ${personName}`);
+    }
+    if (personAge >= 18) {
+        console.log(`студент ${personName} совершеннолетний, ему ${personName}`);
     } else {
-        console.log(`Студент ${college[0]["name"]} не совершеннолетний. Ему ${college[0]["age"]}`);
+        console.log(`Студент ${college[count]["name"]} не совершеннолетний, ему ${college[count]["age"]}`);
+        console.log(`Студент ${personName} не совершеннолетний, ему ${personAge}`);
     }
+
     if (college[0]["cash"] >= 4500) {
-        console.log(`Студент ${college[0]["name"]} получает повышенную стипендию. (${college[0]["cash"]} руб.));
-    } else if ( college[0]["cash"] <= 4400 || college[0]["cash"] >= 1) {
-        console.log(`Студент ${ college[0]["name"] } получает обычную стипендию. (${ college[0]["cash"] } руб.));
-    } else if (college[0]["cash"] == 0) {
-        console.log(`Студент ${college[0]["name"]} не получает стипендию.`);
+        console.log(`Студент ${college[count]["name"]} получает повышенную стипендию, ${college[0]["cash"]} руб.`);
+    } else if (college[0]["cash"] <= 4400 && college[0]["cash"] > 0) {
+        console.log(`Студент ${college[count]["name"]} получает обычную стипендию, ${college[0]["cash"]} руб.`);
+    } else {
+        console.log(`Студент ${college[count]["name"]} не получает стипендию.`);
     }
-} else if (college[0]["isStudent"] === false) {
-    console.log("Преподаватель")
+    if (personcash >= 4500) {
+        console.log(`студент ${personName} получает повышенную стипендию, (${personAge}) руб.`);
+    } else if (personcash <= 4400 && personcash >= 1) {
+        console.log(`студент ${personName} получает обычную стипендию, (${personAge}) руб.`);
+    } else if (personcash == 0) {
+        console.log(`студент ${personName} не получает стипендию`);
+    }
+
+}
+else if (college[0]["isStudent"] === false) {
+    console.log(`Вы выбрали преподавателя ${college[0]["name"]}`);
+    if (college[0]["isKurator"] === true) {
+        console.log(`Преподаватель ${college[0]["name"]} куратор группы ${college[0][iskuratorGroup]}`);
+    } else if (student === false) {
+        console.log(`студент ${personName}`)
+    } if (kurator === true) {
+        console.log(`преподаватель ${personName} куратор группы ${kuratorGroup}`)
+    }
+    console.log("преподаватель")
 }
