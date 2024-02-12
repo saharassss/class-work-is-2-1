544b66aa0121f3e34622a7f6e2588efee912007e
saharassss <sasharasheva@gmail.com>
2024-02-12 11:36:45 +0300

    циклы

---
+++ task_4/main.js
@@ -0,0 +1,26 @@
+// const students = ["tom", "bob", "harry", "ron", "stam"];
+// for (let i = 0; i < students.length; i++) {
+//     let result = students[i];
+//     console.log(result);
+// }
+
+// let people = [["tom", 18], ["Bob", 17], ["stan", 36]];
+// for (let i = 0; i < people.length; i++) {
+//     for (let j = 0; j < people[i].length; j++) {
+//         let result = people[i][j];
+//         console.log(result);
+//     }
+//     console.log("--------");
+// }
+
+// const students = ["tom", "bob", "harry", "ron"];
+
+// for (let item in students) {
+//     console.log(students[item]);
+// }
+
+// const person = {name: "tom", age: 23};
+
+// for (let item in person) {
+//     console.log(item, person[item]);
+// }
